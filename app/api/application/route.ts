e6213ac30b7377fbc1473528a38c750eccfc85da
Murad <skytechnic.music@gmail.com>
2025-03-04 17:23:16 +0300

    first commit

---
+++ app/api/application/route.ts
@@ -0,0 +1,82 @@
+import { NextRequest, NextResponse } from 'next/server';
+import Joi from 'joi';
+import nodemailer from 'nodemailer';
+import fs from "fs";
+import path from "path";
+
+console.log(process.env.GMAIL_USER)
+console.log(process.env.GMAIL_PASS)
+
+const schema = Joi.object({
+  surname: Joi.string().min(1).required(),
+  name: Joi.string().min(1).required(),
+  patronymic: Joi.string().allow('').optional(),
+  birthDate: Joi.date().required(),
+  email: Joi.string().email().required(),
+  occupation: Joi.string().allow('').optional(),
+  conferenceDays: Joi.string().min(1).required(),
+});
+
+const csvFilePath = path.join(process.cwd(), "public", "applications.csv");
+
+export async function POST(
+  request: NextRequest,
+) {
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: process.env.GMAIL_USER, // Ваш Gmail
+      pass: process.env.GMAIL_PASS, // Ваш пароль приложения
+    },
+  });
+
+  try {
+    const body = await request.json();
+    const { error, value } = schema.validate(body);
+  
+    if (error) {
+      return new NextResponse(
+        JSON.stringify({ message: error.details[0].message }),
+        { status: 400, headers: { 'Content-Type': 'application/json' } }
+      );
+    }
+
+    if (!fs.existsSync(csvFilePath)) {
+      fs.writeFileSync(csvFilePath, "Фамилия,Имя,Отчество,Дата рождения,E-mail,Род деятельности,Дни участия в конференции*,Дата заявки\n", "utf8");
+    }
+
+    const newEntry = `${value.name},${value.email},${value.patronymic},${value.birthDate},${value.occupation},${value.conferenceDays},${new Date().toISOString()}\n`;
+
+    fs.appendFileSync(csvFilePath, newEntry, "utf8");
+
+    const mailOptions = {
+      from: process.env.GMAIL_USER,
+      to: process.env.GMAIL_USER, // На какой email отправляется заявка
+      subject: 'Новая заявка с сайта',
+      text: `
+        Имя: ${value.name}
+        Фамилия: ${value.surname}
+        Отчество: ${value.patronymic}
+        Дата рождения: ${value.birthDate}
+        Email: ${value.email}
+        Профессия: ${value.occupation}
+        Дни конференции: ${value.conferenceDays}
+      `,
+    };
+
+
+    // Если все поля валидны
+    await transporter.sendMail(mailOptions);
+    const responseBody = { message: 'Данные успешно валидированы' };
+    return new NextResponse(JSON.stringify(responseBody), { status: 200 });
+  } catch(error) {
+    console.log(error)
+    return new NextResponse(
+      JSON.stringify({ message: 'Ошибка при обработке данных' }),
+      { 
+        status: 400,
+        headers: { 'Content-Type': 'application/json' }
+      },
+    );
+  } 
+};
--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,151 @@
-import Image from "next/image";
+'use client'
 import styles from "./page.module.css";
+import clsx from "clsx";
+import { ChangeEvent, useState } from "react";
+
+const initvalue = {
+  surname: '',
+  name: '',
+  patronymic: '',
+  birthDate: '',
+  email: '',
+  occupation: '',
+  conferenceDays: "16 и 17 апреля",
+}
 
 export default function Home() {
+  const [success, setSuccess] = useState<string | null>(null);
+  const [error, setError] = useState<string | null>(null);
+  const [value, selectValue] = useState(initvalue)
+
+  const changeVields = (e: ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+    selectValue(state => ({
+      ...state,
+      [e.target.name]: e.target.value
+    }))
+  }
+
+  const chageConf = (value: string) => () => {
+    selectValue(state => ({
+      ...state,
+      conferenceDays: value
+    }))
+  }
+
+  const sumbitEvent = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+
+    fetch('/api/application', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(value)
+    }).then((data) => {
+      if (data.status === 400) return setError('Что-то пошло не так...');
+      setSuccess('Ваша завайка принята!');
+    })
+  }
+
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+        <div className={styles.form}>
+          <form onSubmit={sumbitEvent}>
+            <div className={styles.row}>
+              <input
+                type="text"
+                placeholder="Фамилия*"
+                name="surname"
+                value={value.surname}
+                onChange={changeVields}
+              />
+            </div>
+            <div className={styles.row}>
+              <input
+                type="text"
+                placeholder="Имя*"
+                name="name"
+                value={value.name}
+                onChange={changeVields}
+              />
+            </div>
+            <div className={styles.row}>
+              <input
+                type="text"
+                placeholder="Отчество"
+                name="patronymic"
+                value={value.patronymic}
+                onChange={changeVields}
+              />
+            </div>
+            <div className={styles.row}>
+              <input
+                type="date"
+                placeholder="Дата рождения"
+                name="birthDate"
+                value={value.birthDate}
+                onChange={changeVields}
+              />
+            </div>
+            <div className={styles.row}>
+              <input
+                name="email"
+                type="email"
+                placeholder="E-mail*"
+                value={value.email}
+                onChange={changeVields}
+              />
+            </div>
+            <div className={styles.row}>
+              <select name="occupation" value={value.occupation} onChange={changeVields}>
+                <option value="" key="1" selected>Род деятельности</option>
+                <option value="Progerammer" key="2">Progerammer</option>
+                <option value="Designer" key="3">Designer</option>
+                <option value="Other" key="4">Other</option>
+              </select>
+            </div>
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+            <div className={styles.bottom}>
+              <span>Дни участия в конференции*</span>
+              <div className={styles.radio}>
+                <label className={clsx(value.conferenceDays === '16 апреля' && styles.is_active)}>
+                  16 апреля
+                  <input 
+                    type="radio" 
+                    name="conf" 
+                    checked={value.conferenceDays === '16 апреля'}
+                    onChange={chageConf('16 апреля')}
+                  />
+                </label>
+                <label className={clsx(value.conferenceDays === '17 апреля' && styles.is_active)}>
+                  17 апреля
+                  <input 
+                    type="radio"
+                    name="conf"
+                    onChange={chageConf('17 апреля')}
+                    checked={value.conferenceDays === '17 апреля'}
+                  />
+                </label>
+                <label className={clsx(value.conferenceDays === '16 и 17 апреля' && styles.is_active)}>
+                  16 и 17 апреля
+                  <input 
+                    type="radio" 
+                    name="conf" 
+                    onChange={chageConf('16 и 17 апреля')}
+                    checked={value.conferenceDays === '16 и 17 апреля'} 
+                  />
+                </label>
+              </div>
+            </div>
+
+            <button>Зарегистрироваться</button>
+
+            {success && <p className={styles.success}>{success}</p>}
+            {error && <p className={styles.error}>{error}</p>}
+          </form>
         </div>
       </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   );
 }
