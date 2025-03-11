43ab83ea76e7620c0c9605eb8e647e7ab4f90cd0
Murad <skytechnic.music@gmail.com>
2025-03-11 11:27:51 +0300

    fix

--- app/api/application/route.ts
+++ app/api/application/route.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server';
 import Joi from 'joi';
-import nodemailer from 'nodemailer';
+// import nodemailer from 'nodemailer';
 import fileWork from '../utils/file-work';
 
 const schema = Joi.object({
@@ -16,13 +16,13 @@ const schema = Joi.object({
 export async function POST(
   request: NextRequest,
 ) {
-  const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-      user: process.env.GMAIL_USER, // Ваш Gmail
-      pass: process.env.GMAIL_PASS, // Ваш пароль приложения
-    },
-  });
+  // const transporter = nodemailer.createTransport({
+  //   service: 'gmail',
+  //   auth: {
+  //     user: process.env.GMAIL_USER, // Ваш Gmail
+  //     pass: process.env.GMAIL_PASS, // Ваш пароль приложения
+  //   },
+  // });
 
   try {
     const body = await request.json();
@@ -37,25 +37,25 @@ export async function POST(
 
     fileWork(value)
 
-    const mailOptions = {
-      from: `${value.name} ` + process.env.GMAIL_USER,
-      to: process.env.GMAIL_USER, // На какой email отправляется заявка
-      subject: 'Новая заявка с сайта',
-      text: `
-        Имя: ${value.name}
-        Фамилия: ${value.surname}
-        Отчество: ${value.patronymic}
-        Дата рождения: ${value.birthday}
-        Email: ${value.email}
-        Профессия: ${value.occupation}
-        Дни конференции: ${value.date}
-      `,
-      replyTo: value.email,
-    };
+    // const mailOptions = {
+    //   from: `${value.name} ` + process.env.GMAIL_USER,
+    //   to: process.env.GMAIL_USER, // На какой email отправляется заявка
+    //   subject: 'Новая заявка с сайта',
+    //   text: `
+    //     Имя: ${value.name}
+    //     Фамилия: ${value.surname}
+    //     Отчество: ${value.patronymic}
+    //     Дата рождения: ${value.birthday}
+    //     Email: ${value.email}
+    //     Профессия: ${value.occupation}
+    //     Дни конференции: ${value.date}
+    //   `,
+    //   replyTo: value.email,
+    // };
 
 
-    // Если все поля валидны
-    await transporter.sendMail(mailOptions);
+    // // Если все поля валидны
+    // await transporter.sendMail(mailOptions);
     const responseBody = { message: 'Данные успешно валидированы' };
     return new NextResponse(JSON.stringify(responseBody), { status: 200 });
   } catch(error) {
